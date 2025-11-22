4190327a6d7e29231de7b3aa6f03c8462166a6be
Eduardo Vieira <edgvi10@gmail.com>
2025-11-22 08:23:39 -0300

    Added fallback to null path on env

--- src/Controllers/UtilsController.php
+++ src/Controllers/UtilsController.php
@@ -11,19 +11,24 @@ class UtilsController
 
     public function env($path = null)
     {
-        if ($path !== null && file_exists($path)) {
-            $lines = file($path, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
-            foreach ($lines as $line) {
-                if (strpos(trim($line), "#") === 0) continue;
-                list($name, $value) = explode("=", $line, 2);
-                $name = trim($name);
-                $value = trim($value);
-                if (!array_key_exists($name, $_ENV) && !getenv($name)) {
-                    putenv(sprintf("%s=%s", $name, $value));
-                    $_ENV[$name] = $value;
-                }
-            }
-        }
+        if ($path === null) $path = ".env";
+
+        if (!file_exists($path))
+            throw new \Exception("File not found: $path");
+
+        $lines = file($path, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+        foreach ($lines as $line) :
+            if (strpos(trim($line), "#") === 0) continue;
+            list($name, $value) = explode("=", $line, 2);
+            $name = trim($name);
+            $value = trim($value);
+            if (!array_key_exists($name, $_ENV) && !getenv($name)) :
+                putenv(sprintf("%s=%s", $name, $value));
+                $_ENV[$name] = $value;
+            endif;
+        endforeach;
+
+        return true;
     }
 
     public static function getEnvironmentInfo()
